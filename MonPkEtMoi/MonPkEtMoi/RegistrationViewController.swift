b7debd9e8df771d58b5f70cf47d5d8c567418b34
Matthieu DYE <matthieu.dye@igtp4-mac01.isim.intra>
2018-03-13 18:00:51 +0100

    [Registration]Links between view and controller

--- MonPkEtMoi/MonPkEtMoi/RegistrationViewController.swift
+++ MonPkEtMoi/MonPkEtMoi/RegistrationViewController.swift
@@ -12,7 +12,16 @@ class RegistrationViewController : UIViewController {
     
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var dateBirthTextField: UITextField!
+    @IBOutlet weak var dateBirthDatePicker: UIDatePicker!
+    @IBOutlet weak var adressTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var postalCodeTextField: UITextField!
    
+    @IBAction func createAccountButton(_ sender: Any) {
+        
+    }
+    
     override internal func viewDidLoad() {
         
     }
