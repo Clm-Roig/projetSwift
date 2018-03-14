5b45462ac0486428f983f7aa460467cfc144ec64
Clement ROIG <clement.roig@igtp4-mac08.isim.intra>
2018-03-14 08:54:56 +0100

    creating models

---
+++ MonPkEtMoi/MonPkEtMoi/Models/Treatment.swift
@@ -0,0 +1,13 @@
+//
+//  Treatment.swift
+//  MonPkEtMoi
+//
+//  Created by Clement ROIG on 14/03/2018.
+//  Copyright © 2018 Roig-Dye. All rights reserved.
+//
+
+import Foundation
+
+extension Treatment {
+    
+}
---
+++ MonPkEtMoi/MonPkEtMoi/PersonModel.swift
@@ -0,0 +1,16 @@
+//
+//  PersonModel.swift
+//  MonPkEtMoi
+//
+//  Created by Clement ROIG on 14/03/2018.
+//  Copyright © 2018 Roig-Dye. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Person {
+    
+
+    
+}
