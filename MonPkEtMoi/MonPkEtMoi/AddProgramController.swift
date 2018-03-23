7a9515760bef0461bb3febd162c04aed5d1e1f42
Clement ROIG <clement.roig@igtp4-mac07.isim.intra>
2018-03-23 09:30:16 +0100

    Add prog

--- MonPkEtMoi/MonPkEtMoi/AddProgramController.swift
+++ MonPkEtMoi/MonPkEtMoi/AddProgramController.swift
@@ -10,7 +10,60 @@ import Foundation
 import UIKit
 
 class AddProgramController: UIViewController {
+    let exerciseDAO = CoreDataDAOFactory.getInstance().getExerciseDAO()
+
+    @IBOutlet weak var activityTF: UITextField!
+    @IBOutlet weak var duration: UILabel!
+    @IBOutlet weak var frequency: UILabel!
+    
+    @IBAction func durationStepperTouched(_ sender: UIStepper) {
+        self.duration.text = String(Int(sender.value))
+    }
     
     
+    @IBAction func frequencyStepperTouched(_ sender: UIStepper) {
+        self.frequency.text = String(Int(sender.value))
+    }
+    
+    @IBAction func addProgram(_ sender: Any) {
+        if(activityTF.text != "") {
+            var newProgram = Program()
+            var newExercise = Exercise()
+            do {
+                newProgram = try exerciseDAO.createProgram()
+            } catch {
+                AlertHelper.alertError(view: self, errorMessage: "Erreur à la création du programme.")
+            }
+            
+            do {
+                newExercise = try exerciseDAO.create()
+            } catch {
+                AlertHelper.alertError(view: self, errorMessage: "Erreur à la création de l'exercice.")
+            }
+            
+            newExercise.wording = activityTF.text
+            newProgram.isComposedBy = newExercise
+            newProgram.duration = Int16(duration.text!)!
+            
+            do {
+                try exerciseDAO.save()
+            } catch {
+                AlertHelper.alertError(view: self, errorMessage: "Erreur à la sauvegarde du programme.")
+            }
+        }
+        else {
+            AlertHelper.alertWarning(view: self, warningMessage: "Veuillez renseigner une activité.")
+        }
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        frequency.text = "1"
+        duration.text = "5"
+        
+    }
     
 }
