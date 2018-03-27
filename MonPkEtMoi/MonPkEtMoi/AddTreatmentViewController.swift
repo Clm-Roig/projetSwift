e1daa86195b8acfbe7b954a979d79521a6a1edd4
Clement ROIG <clement.roig@igtp3-mac12.isim.intra>
2018-03-27 16:50:24 +0200

    add treatment

--- MonPkEtMoi/MonPkEtMoi/AddTreatmentViewController.swift
+++ MonPkEtMoi/MonPkEtMoi/AddTreatmentViewController.swift
@@ -13,15 +13,61 @@ class AddTreatmentViewController: UIViewController, UIPickerViewDataSource, UIPi
     let treatmentDAO = CoreDataDAOFactory.getInstance().getTreatmentDAO()
     var medicines: [Medicine?] = []
     var newTreatment: Treatment?
-    var hoursTableViewCtrl: AddTreatmentHourTableViewController?
     
     @IBOutlet weak var medicinePicker: UIPickerView!
     @IBOutlet weak var quantityTF: UITextField!
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var minuteLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
 
+
+    @IBAction func hourStepperPressed(_ sender: UIStepper) {
+        self.hourLabel.text = String(Int(sender.value))
+    }
+    
+    @IBAction func minuteStepperPressed(_ sender: UIStepper) {
+        self.minuteLabel.text = String(Int(sender.value))
+    }
+
+    
+    
+    @IBAction func addTreatment(_ sender: Any) {
+        if(self.quantityTF.text != "") {
+            var newTreatment = Treatment()
+            do {
+                newTreatment = try treatmentDAO.create()
+            } catch {
+                AlertHelper.alertError(view: self, errorMessage: "Erreur à la création du nouveau traitement.")
+            }
+            newTreatment.beginningDate = Date() as NSDate
+            newTreatment.endingDate = datePicker.date as NSDate
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm"
+            let hour = formatter.date(from: hourLabel.text! + ":" + minuteLabel.text!)
+            newTreatment.hours = [hour! as NSDate];
+            newTreatment.quantity = quantityTF.text
+            newTreatment.need = medicines[medicinePicker.selectedRow(inComponent: 0)]
+            
+            do {
+                try treatmentDAO.save()
+            } catch {
+                AlertHelper.alertError(view: self, errorMessage: "Erreur à la sauvegarde du traitement.")
+            }
+            
+        }
+        else {
+            AlertHelper.alertWarning(view: self, warningMessage: "Veuillez saisir une posologie.")
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.frequencyL.text = "1"
+        
+        self.datePicker.minimumDate = Date()
+
         do {
             self.medicines = try treatmentDAO.getAllMedicines()
         } catch {
@@ -39,11 +85,6 @@ class AddTreatmentViewController: UIViewController, UIPickerViewDataSource, UIPi
                 return false
             }
         }
-        
-        self.hoursTableViewCtrl = AddTreatmentHourTableViewController(tableView: hoursTableView)
-        
-        self.hoursTableView.dataSource = hoursTableViewCtrl
-        self.hoursTableView.delegate = hoursTableViewCtrl
        
     }
     
