2056a2b73f1aea81d3fe9254278714d91b04fee5
Thais Aurard <thais.aurard@etu.umontpellier.fr>
2018-03-31 17:40:56 +0200

    Notif - save intake ; Agenda - fix treatment display

--- MonPkEtMoi/MonPkEtMoi/NotificationDelegate.swift
+++ MonPkEtMoi/MonPkEtMoi/NotificationDelegate.swift
@@ -33,6 +33,31 @@ class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
             print("Taken")
             let medicationIntakeDAO = CoreDataDAOFactory.getInstance().getMedicationIntakeDAO()
             
+            // Retrieve the delay of the medication intake and insert the medication intake
+            if let delay = response.notification.request.content.userInfo["delay"] as? String {
+                var medicationIntake: MedicationIntake?
+                do {
+                    medicationIntake = try medicationIntakeDAO.create()
+                } catch let error {
+                    print("Erreur à la création de la prise de médicament.")
+                    print(error)
+                }
+                
+                guard let newMedicationIntake = medicationIntake else {
+                    return
+                }
+                medicationIntake = newMedicationIntake
+                medicationIntake?.date = Date() as NSDate
+                medicationIntake?.delay = Int16(delay)!
+                
+                // insert in DB
+                do {
+                    try medicationIntakeDAO.save()
+                } catch let error {
+                    print("Erreur à la sauvegarde de la prise de médicament")
+                    print(error)
+                }
+            }
 
         default:
             print("Unknown action")
